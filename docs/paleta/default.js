545f3ead78e21537841a457b63b2de36b7e89d64
Feiron <53085628+fr1g@users.noreply.github.com>
2023-11-20 22:16:51 +0800

    trying add new functions

---
+++ docs/paleta/default.js
@@ -0,0 +1,93 @@
+var default_colors = [
+    {
+        name: "White",
+        hex: "#FFFFFF",
+        rgb: "255, 255, 255",
+        text: "black",
+        poster: "frig",
+        contact: ""
+    },
+    {
+        name: "Black",
+        hex: "#000000",
+        rgb: "0, 0, 0",
+        text: "ghostwhite",
+        poster: "frig",
+        contact: ""
+    },
+    {
+        name: "Slate",
+        hex: "#e2e8f0",
+        rgb: "?",
+        text: "#020617",
+        poster: "Tailwind",
+        contact: "https://tailwindcss.com"
+    },
+    {
+        name: "Gray",
+        hex: "#e5e7eb",
+        rgb: "?",
+        text: "#030712",
+        poster: "Tailwind",
+        contact: "https://tailwindcss.com"
+    },
+    {
+        name: "Zinc",
+        hex: "#e4e4e7",
+        rgb: "?",
+        text: "#09090b",
+        poster: "Tailwind",
+        contact: "https://tailwindcss.com"
+    },
+    {
+        name: "Neutral",
+        hex: "#e5e5e5",
+        rgb: "?",
+        text: "#0a0a0a",
+        poster: "Tailwind",
+        contact: "https://tailwindcss.com"
+    },
+    {
+        name: "Stone",
+        hex: "#e7e5e4",
+        rgb: "?",
+        text: "#0c0a09",
+        poster: "Tailwind",
+        contact: "https://tailwindcss.com"
+    },
+    {
+        name: "Red/Fecaca",
+        hex: "#fecaca",
+        rgb: "?",
+        text: "#450a0a",
+        poster: "Tailwind",
+        contact: "https://tailwindcss.com"
+    },
+    {
+        name: "Oránge",
+        hex: "#fed7aa",
+        rgb: "?",
+        text: "#431407",
+        poster: "Tailwind",
+        contact: "https://tailwindcss.com"
+    },
+    {
+        name: "Rose Feint",
+        hex: "?",
+        rgb: "222, 168, 165",
+        text: "#431407",
+        poster: "藤原",
+        contact: "-"
+    },
+];
+const usageText = `Here's an input bar, and you can input: \n
+HEX like #3f3f3f or HEXA like #3f3f3f0A; \n
+or \n
+RGB like 39, 39, 39(recommended) // rgb(39, 39, 39) or RGBA like (11, 45, 14, 0.1919810). \n
+After the Colour Code, you can use :: to add parameters after. \n
+The parameter should be a colour, or CSS colour code. \n
+This will be the Colour of preview text, as a reference of colour matching. \n
+Mouse hovering over each content of card, you can see the info of each part. \n
+Like poster info, colour code explain, and match-colour of text. \n
+You can click on every Colour Code contained in the cards to copy the code. \n
+This tips will disappear in 10 seconds, and you can shut it immediately by Refresh the Page. `;
